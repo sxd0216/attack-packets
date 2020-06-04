9b29412b00b96cb4e66cd9d122922384691bf58f
sxd0216 <65489371+sxd0216@users.noreply.github.com>
2020-06-04 11:38:42 +0800

    Add files via upload

---
+++ start.py
@@ -0,0 +1,36 @@
+'''
+copyright:..
+author:..
+date:..
+description:..
+'''
+
+
+import socket
+import time
+import getopt
+import sys
+import struct
+import os
+
+    
+if __name__ == "__main__":    
+    dstport = 502   
+    dstip ="192.168.180.146"     
+    addr = (dstip,dstport)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.connect(addr)
+
+    ##(Write coil)
+    sendstr1="\x00\x00\x00\x00\x00\x06\x01\x05\x00\x03\xff\x00"
+    sendstr2="\x00\x00\x00\x00\x00\x06\x01\x05\x00\x01\x00\x00"
+   
+    sock.send(sendstr1)
+    sock.send(sendstr2)
+    print sock.recv(1024000)
+
+   
+    
+    sock.close()       
+
+            
